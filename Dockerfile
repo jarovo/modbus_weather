1e46c9f64ee779d37eafcd61ca0f3a865db1db20
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2023-03-07 00:15:46 +0100

    Build-system in docker/podman.

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,27 @@
+# syntax=docker/dockerfile:1
+
 # Create Dockerfile for python with cashed dnf and pip installed packages
-FROM python:3.10-alpine
+FROM python:3.10-alpine AS runner
 
 # Install git and pip packages
 RUN apk add git
 COPY requirements.txt /tmp/
+
 RUN pip install -r /tmp/requirements.txt
 
+# Run application
+ENTRYPOINT [ "python", "app.py" ]
+
 # Copy source code
+#VOLUME /app
 COPY . /app
 WORKDIR /app
 
-# Run application
-ENTRYPOINT [ "python", "app.py"]
+FROM runner AS builder
+# Copy the build requirements
+COPY requirements.build.txt /tmp/
+RUN pip install -r /tmp/requirements.build.txt
+
+RUN pip install pre-commit
+COPY .pre-commit-config.yaml .
+RUN git init . && pre-commit install-hooks
--- app.py
+++ app.py
@@ -12,30 +12,42 @@ from pymodbus.datastore import (
     ModbusSlaveContext,
 )
 from pymodbus.constants import Endian
-from pymodbus.payload import (BinaryPayloadDecoder, BinaryPayloadBuilder)
+from pymodbus.payload import BinaryPayloadDecoder, BinaryPayloadBuilder
 
 _logger = logging.getLogger()
 
-OPENWEATHERMAP_API='https://api.openweathermap.org/data/2.5/weather?'
+OPENWEATHERMAP_API = "https://api.openweathermap.org/data/2.5/weather?"
+
 
 def make_args_parser():
-    parser = argparse.ArgumentParser(description='OpenWeatherAPI to modbus adapter.')
-    parser.add_argument('api_key', help='Openweather API key.')
-    parser.add_argument('--log', default='info',
-                        choices=['critical', 'error', 'warning', 'info', 'debug'],
-                        help='Log level')
-    parser.add_argument('--modbus-listen-addres', dest='address', default='0.0.0.0',
-                        help='Address for the Modbus slave (server) to listen on.')
-    parser.add_argument('--modbus-listen-port', dest='port', default='502',
-                        help='Modbus slave (server) port.')
+    parser = argparse.ArgumentParser(description="OpenWeatherAPI to modbus adapter.")
+    parser.add_argument("api_key", help="Openweather API key.")
+    parser.add_argument(
+        "--log",
+        default="info",
+        choices=["critical", "error", "warning", "info", "debug"],
+        help="Log level",
+    )
+    parser.add_argument(
+        "--modbus-listen-addres",
+        dest="address",
+        default="0.0.0.0",
+        help="Address for the Modbus slave (server) to listen on.",
+    )
+    parser.add_argument(
+        "--modbus-listen-port",
+        dest="port",
+        default="502",
+        help="Modbus slave (server) port.",
+    )
     return parser
 
 
 getters = {
-    'main': ('temp', 'pressure', 'humidity'),
-    'wind': ("speed", "deg", "gust"),
-    'clouds': ("all",),
-    'sys': ("sunrise", "sunset")
+    "main": ("temp", "pressure", "humidity"),
+    "wind": ("speed", "deg", "gust"),
+    "clouds": ("all",),
+    "sys": ("sunrise", "sunset"),
 }
 
 
@@ -45,8 +57,10 @@ def get_lat_lon():
 
 def make_openweathermap_request(args):
     lat, lon = get_lat_lon()
-    resp=requests.get(OPENWEATHERMAP_API, params=dict(lat=lat, lon=lon, appid=args.api_key)).json()
-    _logger.debug(f'openweatherapi response: {resp}')
+    resp = requests.get(
+        OPENWEATHERMAP_API, params=dict(lat=lat, lon=lon, appid=args.api_key)
+    ).json()
+    _logger.debug(f"openweatherapi response: {resp}")
     return resp
 
 
@@ -59,13 +73,16 @@ def friendly_itemgetter(*items):
                 return obj[item]
             except KeyError as exc:
                 raise KeyError(f"{exc} when processing {obj}")
+
     else:
         try:
+
             def g(obj):
                 return tuple(obj[item] for item in items)
+
         except KeyError as exc:
             raise Exception(f"{exc.msg} when processing {obj}")
-        
+
     return g
 
 
@@ -74,15 +91,17 @@ def tuplify(*items):
         return tuple(*items)
     except TypeError:
         # The items is not an iterable.
-        return tuple(items,)
+        return tuple(
+            items,
+        )
 
 
 def extract_vals(resp):
     vals = []
     for key, items in getters.items():
-        _logger.debug(f'{key}, {items}')
+        _logger.debug(f"{key}, {items}")
         vals.extend(tuplify(friendly_itemgetter(*items)(resp[key])))
-    _logger.debug(f'values: {vals}')
+    _logger.debug(f"values: {vals}")
     return vals
 
 
@@ -108,7 +127,7 @@ async def updating_task(args):
         fc_as_hex = 3
         slave_id = 0x00
         address = 0x10
-        #values = context[slave_id].getValues(fc_as_hex, address, count=3)
+        # values = context[slave_id].getValues(fc_as_hex, address, count=3)
         openweather_api_vals = await get_weather_values(args)
         values = convert_ints_to_floats(openweather_api_vals)
         txt = f"new values: {str(values)}"
@@ -142,7 +161,7 @@ async def run_updating_server(args):
 if __name__ == "__main__":
     parser = make_args_parser()
     cmd_args = parser.parse_args()
-    cmd_args.comm = 'tcp'
+    cmd_args.comm = "tcp"
     cmd_args.framer = None
     run_args = setup_updating_server(cmd_args)
     asyncio.run(run_updating_server(run_args), debug=True)
